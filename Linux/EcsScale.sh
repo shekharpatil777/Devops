04dcd4920cebfbf3b6d07b47f2a4a8d30e11b695
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-01-03 12:15:26 +0530

    Update added ecs services scale script

---
+++ Linux/EcsScale.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Set the desired count for scaling up
+DESIRED_COUNT=2 # Adjust this value as needed
+
+# Get a list of all services in the cluster
+SERVICES=$(aws ecs list-services --cluster <YOUR_CLUSTER_NAME> --query 'serviceArns[]' --output text)
+
+# Iterate through each service and update its desired count
+for SERVICE in $SERVICES; do
+  aws ecs update-service --cluster <YOUR_CLUSTER_NAME> --service "$SERVICE" --desired-count "$DESIRED_COUNT"
+done
+
+echo "All services in cluster <YOUR_CLUSTER_NAME> scaled up to $DESIRED_COUNT instances."
