614cd442959e5cc98e714d20eb6c552e5d45059f
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-01-12 22:43:42 +0530

    Update Linux added MySQL file

---
+++ Linux/run_mysql.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+read -p "MySQL User: " user_name
+read -sp "MySQL Password: " mysql_pwd
+echo
+read -p "MySQL Command: " mysql_cmd
+read -p "MySQL DB: "  mysql_db
+mysql -u "$user_name" -p$mysql_pwd $mysql_db -Be"$mysql_cmd"
