8bf3cd4cf322c47821498b7e189d730ceb688457
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-01-12 22:43:56 +0530

    Update run_mysql.sh

--- Linux/run_mysql.sh
+++ Linux/run_mysql.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Script to prompt for MYSQL user password and database
+# MySQL client is used to make the connection to the localhost
+# The query collect from the prompt is executed
+
 
 read -p "MySQL User: " user_name
 read -sp "MySQL Password: " mysql_pwd
