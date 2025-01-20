b639a00428f6b96e806fb4fb2e2ab25a5a18d474
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-01-20 23:43:04 +0530

    Update Linux

---
+++ Linux/logrm.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+
+removelog="/tmp/removelog.log"
+
+if [ $# -eq 0 ] ; then
+  echo "Usage: $0 [-s] list of files or directories" >&2
+  exit 1
+fi
+
+if [ "$1" = "-s" ] ; then
+  # silent operation requested... don't log
+  shift
+else
+  echo "$(date): ${USER}: $@" >> $removelog
+fi
+
+/bin/rm "$@"     
+
+exit 0
