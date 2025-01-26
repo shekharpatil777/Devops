e3d595830040b181005dd44d61313bb803006ba3
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-01-26 19:25:37 +0530

    Update Linux

---
+++ Linux/remember.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+rememberfile="$HOME/.remember"
+
+if [ $# -eq 0 ] ; then
+  echo "Enter note, end with ^D: "
+  cat - >> $rememberfile
+else
+  echo "$@" >> $rememberfile
+fi
+
+exit 0
