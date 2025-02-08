3b24a801009d88de7dcab492db272ee5be10b3bf
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-02-08 13:48:56 +0530

    Update Linux

---
+++ Linux/too_long.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+
+width=72
+
+if [ ! -r "$1" ] ; then
+  echo "Usage: $0 filename" >&2; exit 1
+fi
+
+while read input
+  do
+     if [ ${#input} -gt $width ] ; then
+       echo "$input" | fmt 
+     else
+       echo "$input"
+     fi
+  done < $1
+
+exit 0
