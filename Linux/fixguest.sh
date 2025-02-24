1075941978fc31adbd923c3a5c816ab59672cfbb
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-02-24 16:40:12 +0530

    Update Linux

---
+++ Linux/fixguest.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+
+
+iam="$(whoami)"
+myhome="$(grep "^${iam}:" /etc/passwd | cut -d: -f6)"
+
+# *** Do NOT run this script on a regular user account!
+
+if [ "$iam" != "guest" ] ; then
+  echo "Error: you really don't want to run fixguest on this account." >&2
+  exit 1
+fi
+
+if [ ! -d $myhome/..template ] ; then
+  echo "$0: no template directory found for rebuilding." >&2
+  exit 1
+fi
+
+# remove all files and directories in the home account
+
+cd $myhome
+
+rm -rf * $(find . -name ".[a-zA-Z0-9]*" -print)
+
+# now the only thing present should be the ..template directory
+
+cp -Rp ..template/* .
+
+exit 0
