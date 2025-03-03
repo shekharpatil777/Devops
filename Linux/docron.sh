482f4e19f20947f363171a1aa3e3948dc0a0846e
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-03-03 20:32:18 +0530

    Update Linux

---
+++ Linux/docron.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+
+rootcron="/etc/crontab"
+
+if [ $# -ne 1 ] ; then
+  echo "Usage: $0 [daily|weekly|monthly]" >&2
+  exit 1
+fi
+
+if [ "$(id -u)" -ne 0 ] ; then
+  echo "$0: Command must be run as 'root'" >&2
+  exit 1
+fi
+
+job="$(awk "NR > 6 && /$1/ { for (i=7;i<=NF;i++) print \$i }" $rootcron)"
+
+if [ -z $job ] ; then
+  echo "$0: Error: no $1 job found in $rootcron" >&2
+  exit 1
+fi
+
+SHELL=/bin/sh                # to be consistent with cron's default
+
+eval $job
