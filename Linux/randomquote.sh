16dc763dbbe0460f49b190d2755344e091666876
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-03-24 23:24:21 +0530

    Create randomquote.sh

---
+++ Linux/randomquote.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+
+awkscript="/tmp/randomquote.awk.$$"
+
+if [ $# -ne 1 ] ; then
+  echo "Usage: randomquote datafilename" >&2
+  exit 1
+elif [ ! -r "$1" ] ; then
+  echo "Error: quote file $1 is missing or not readable" >&2
+  exit 1
+fi
+
+trap "/bin/rm -f $awkscript" 0
+
+cat << "EOF" > $awkscript
+BEGIN { srand(); }
+      { s[NR] = $0 } 
+END   { print s[randint(NR)] } 
+function randint(n) { return int (n * rand() ) + 1 }
+EOF
+
+awk -f $awkscript < "$1"
+
+exit 0
