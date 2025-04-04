08509fb5abe7dc0ea6fc4984dc9476a9219b20cc
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-04-04 22:21:23 +0530

    Create trimmailbox.sh

---
+++ Linux/trimmailbox.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+
+
+keep=4	# by default, let's just keep around the four most recent messages
+
+totalmsgs="$(echo 'x' | mail | sed -n '2p' | awk '{print $2}')"
+
+if [ $totalmsgs -lt $keep ] ; then
+  exit 0          # nothing to do
+fi
+
+topmsg="$(( $totalmsgs - $keep ))"
+
+mail > /dev/null << EOF
+d1-$topmsg
+q
+EOF
+
+exit 0
