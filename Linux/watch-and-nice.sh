868efcea93462213ee9970eeff9a0e85d6011915
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-04-10 22:37:58 +0530

    Create watch-and-nice.sh

---
+++ Linux/watch-and-nice.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+
+
+renicename="$HOME/bin/renicename"
+
+if [ $# -ne 2 ] ; then
+  echo "Usage: $(basename $0) desirednice jobname" >&2
+  exit 1
+fi
+
+pid="$($renicename -p "$2")"
+
+if [ ! -z "$(echo $pid | sed 's/[0-9]*//g')" ] ; then
+  echo "Failed to make a unique match in the process table for $2" >&2
+  exit 1
+fi
+
+currentnice="$(ps -lp $pid | tail -1 | awk '{print $6}')"
+
+if [ $1 -gt $currentnice ] ; then
+  echo "Adjusting priority of $2 to $1"
+  renice $1 $pid
+fi
+
+exit 0
