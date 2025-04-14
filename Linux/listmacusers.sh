6efca01cb8adda9adf98cf3d83f00467007cd5f0
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-04-14 23:43:48 +0530

    Create listmacusers.sh

---
+++ Linux/listmacusers.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+
+
+fields=""
+
+while getopts "Aahnprsu" opt ; do
+  case $opt in
+    A ) fields="uid passwd name realname home shell"	;;
+    a ) fields="uid name realname home shell"		;;
+    h ) fields="$fields home"				;;
+    n ) fields="$fields name"				;;
+    p ) fields="$fields passwd"				;;
+    r ) fields="$fields realname"			;;
+    s ) fields="$fields shell"				;;
+    u ) fields="$fields uid"				;;
+    ? ) cat << EOF >&2
+Usage: $0 [A|a|hnprsu]
+Where:
+   -A    output all known NetInfo user fields
+   -a    output only the interesting user fields
+   -h    show home directories of accounts
+   -n    show account names
+   -p    passwd (encrypted)
+   -r    show realname/fullname values
+   -s    show login shell
+   -u    uid
+EOF
+exit 1
+  esac
+done
+
+exec nireport . /users ${fields:=uid name realname home shell}
