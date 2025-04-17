6b1da6edf437f552b8f60d3160349e8bfbb4281b
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-04-17 18:12:54 +0530

    Update titleterm.sh

--- Linux/titleterm.sh
+++ Linux/titleterm.sh
@@ -1,6 +1,12 @@
 #! /bin/sh
 
+# titleterm - tell the Mac OS X Terminal application to change its name
+#   to the value specified as an argument to this succinct script.
 
+# To use this to show your current working directory, for example:
+#    alias precmd 'titleterm "$PWD"'			[tcsh]
+# or
+#    export PROMPT_COMMAND="titleterm \"\$PWD\"" 	[bash]
 
 if [ $# -eq 0 ]; then
   echo "Usage: $0 title" >&2
