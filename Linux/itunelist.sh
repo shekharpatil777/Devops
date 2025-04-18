414dbb2ff69ce7d0b14764942b30fbb588a40f9e
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-04-18 20:09:52 +0530

    Create itunelist.sh

---
+++ Linux/itunelist.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+itunehome="$HOME/Music/iTunes"
+ituneconfig="$itunehome/iTunes Music Library.xml"
+
+musiclib="/$(grep '>Music Folder<' "$ituneconfig" | cut -d/ -f5- | \
+   cut -d\< -f1 | sed 's/%20/ /g')"
+
+echo Your music library is at $musiclib
+
+if [ ! -d "$musiclib" ] ; then
+  echo "$0: Confused: Music library $musiclib isn't a directory?" >&2
+  exit 1
+fi
+
+exec find "$musiclib" -type d -mindepth 2 -maxdepth 2 \! -name '.*' -print | sed "s|$musiclib/||"
