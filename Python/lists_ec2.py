27e53c549cc8f47b7d7d3c04f248ff6585e0d222
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-05-18 21:47:35 +0530

    Update lists_ec2.py

--- Python/lists_ec2.py
+++ Python/lists_ec2.py
@@ -1,5 +1,7 @@
 import boto3
 
+#This script shows how to list EC2 instances in a specific region. You'll need to have the AWS SDK (Boto3) installed (pip install boto3) and your AWS credentials configured.
+
 def list_ec2_instances(region="ap-south-1"):
     ec2 = boto3.client('ec2', region_name=region)
     response = ec2.describe_instances()
