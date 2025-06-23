65daf4ba609d694c19b936cf7a59422441cc750c
chandrashekhar patil <63929526+shekharpatil777@users.noreply.github.com>
2025-06-23 23:18:22 +0530

    Update displaytime.py

--- Python/displaytime.py
+++ Python/displaytime.py
@@ -3,7 +3,7 @@ from datetime import datetime
 # Get current time
 now = datetime.now()
 
-
+# Format time as HH:MM:SS
 current_time = now.strftime("%H:%M:%S")
 
 print("Current Time:", current_time)
